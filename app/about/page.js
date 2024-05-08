5f26fb22ec164240f50fbd345c5d411f017a3848
Hamid Raza <itmughal2023@gmail.com>
2024-05-08 18:36:37 +0500

    Add export default particular functions in (about, features, contact).js

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="container">
+            <h1 className={`mt-3 mb-2`}>About</h1>
+        </div>
+    );
+}
---
+++ app/features/page.js
@@ -0,0 +1,7 @@
+export default function Features() {
+    return (
+        <div className="container">
+            <h1 className={`mt-3 mb-2`}>Features</h1>
+        </div>
+    );
+}
