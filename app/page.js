52b4460c6c6bf3841a792458e969602f2b6ae2f3
Hamid Raza <itmughal2023@gmail.com>
2024-05-08 16:56:10 +0500

    Remove Palypti Font

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import NotesList from "./Components/NotesList";
-import { Platypi } from 'next/font/google'
-const Platypi_Font = Platypi({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="container">
-      <h1 className={`${Platypi_Font.className} mt-3 mb-2`}>Your Notes</h1>
+      <h1 className={`mt-3 mb-2`}>Your Notes</h1>
       <NotesList />
     </div>
   );
